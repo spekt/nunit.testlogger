d1c7ecd740067ba34c7e116b9570b7fdad425587
Marko Lahma <marko.lahma@finbiosoft.com>
2017-08-18 08:04:16 +0300

    fixes #7 Add a logger for NUnit XML format

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Reporting extensions for [Visual Studio Test Platform](https://gtihub.com/micros
 | Logger | Nuget Package |
 | ------ | ------------- |
 | AppVeyor | [![NuGet](https://img.shields.io/nuget/v/Appveyor.TestLogger.svg)](https://www.nuget.org/packages/Appveyor.TestLogger/) |
+| NUnit | [![NuGet](https://img.shields.io/nuget/v/NUnitXml.TestLogger.svg)](https://www.nuget.org/packages/NUnitXml.TestLogger/) |
 | Xunit | [![NuGet](https://img.shields.io/nuget/v/XunitXml.TestLogger.svg)](https://www.nuget.org/packages/XunitXml.TestLogger/) |
 
 ## Usage
@@ -19,6 +20,24 @@ The appveyor logger can report test results automatically to the CI build. See a
 > dotnet test --test-adapter-path:. --logger:Appveyor
 ```
 3. Test results are automatically reported to the AppVeyor CI results
+
+
+### NUnit Logger
+NUnit logger can generate xml reports in the NUnit v3 format (https://github.com/nunit/docs/wiki/Test-Result-XML-Format).
+
+1. Add a reference to the [NUnit Logger](https://www.nuget.org/packages/NUnitXml.TestLogger) nuget package in test project
+2. Use the following command line in tests
+```
+> dotnet test --test-adapter-path:. --logger:nunit
+```
+3. Test results are generated in the `TestResults` directory relative to the `test.csproj`
+
+A path for the report file can be specified as follows:
+```
+> dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=loggerFile.xml
+```
+
+`loggerFile.xml` will be generated in the same directory as `test.csproj`.
  
 ### Xunit Logger
 Xunit logger can generate xml reports in the xunit v2 format (https://xunit.github.io/docs/format-xml-v2.html).
---
+++ src/NUnit.Xml.TestLogger/NUnitXmlTestLogger.cs
@@ -0,0 +1,501 @@
+﻿namespace Microsoft.VisualStudio.TestPlatform.Extension.NUnit.Xml.TestLogger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using ObjectModel;
+    using ObjectModel.Client;
+    using ObjectModel.Logging;
+
+    [FriendlyName(FriendlyName)]
+    [ExtensionUri(ExtensionUri)]
+    public class NUnitXmlTestLogger : ITestLoggerWithParameters
+    {
+        /// <summary>
+        /// Uri used to uniquely identify the logger.
+        /// </summary>
+        public const string ExtensionUri = "logger://Microsoft/TestPlatform/NUnitXmlLogger/v1";
+
+        /// <summary>
+        /// Alternate user friendly string to uniquely identify the console logger.
+        /// </summary>
+        public const string FriendlyName = "nunit";
+
+        public const string LogFilePathKey = "LogFilePath";
+        public const string EnvironmentKey = "Environment";
+        public const string NUnitVersionKey = "NUnitVersion";
+
+        private const string ResultStatusPassed = "Passed";
+        private const string ResultStatusFailed = "Failed";
+
+        private string outputFilePath;
+        private string environmentOpt;
+        private string nunitVersionOpt;
+
+        private readonly object resultsGuard = new object();
+        private List<TestResultInfo> results;
+        private DateTime localStartTime;
+
+        private class TestResultInfo
+        {
+            private readonly TestResult result;
+            public TestCase TestCase => result.TestCase;
+            public TestOutcome Outcome => result.Outcome;
+            public string AssemblyPath => result.TestCase.Source;
+            public readonly string Type;
+            public readonly string Method;
+            public string Name => result.TestCase.DisplayName;
+            public TimeSpan Time => result.Duration;
+            public string ErrorMessage => result.ErrorMessage;
+            public string ErrorStackTrace => result.ErrorStackTrace;
+            public IReadOnlyCollection<TestResultMessage> Messages => result.Messages;
+            public TraitCollection Traits => result.Traits;
+
+            public TestResultInfo(
+                TestResult result,
+                string type,
+                string method)
+            {
+                this.result = result;
+                Type = type;
+                Method = method;
+            }
+
+            public override int GetHashCode()
+            {
+                return result.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is TestResultInfo)
+                {
+                    TestResultInfo objectToCompare = (TestResultInfo) obj;
+                    if (string.Compare(ErrorMessage, objectToCompare.ErrorMessage) == 0
+                        && string.Compare(ErrorStackTrace, objectToCompare.ErrorStackTrace) == 0)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public void Initialize(TestLoggerEvents events, string testResultsDirPath)
+        {
+            if (events == null)
+            {
+                throw new ArgumentNullException(nameof(events));
+            }
+
+            if (testResultsDirPath == null)
+            {
+                throw new ArgumentNullException(nameof(testResultsDirPath));
+            }
+
+            var outputPath = Path.Combine(testResultsDirPath, "TestResults.xml");
+            InitializeImpl(events, outputPath);
+        }
+
+        public void Initialize(TestLoggerEvents events, Dictionary<string, string> parameters)
+        {
+            if (events == null)
+            {
+                throw new ArgumentNullException(nameof(events));
+            }
+
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
+            if (parameters.TryGetValue(LogFilePathKey, out string outputPath))
+            {
+                InitializeImpl(events, outputPath);
+            }
+            else if (parameters.TryGetValue(DefaultLoggerParameterNames.TestRunDirectory, out string outputDir))
+            {
+                Initialize(events, outputDir);
+            }
+            else
+            {
+                throw new ArgumentException($"Expected {LogFilePathKey} or {DefaultLoggerParameterNames.TestRunDirectory} parameter", nameof(parameters));
+            }
+
+            parameters.TryGetValue(EnvironmentKey, out environmentOpt);
+            parameters.TryGetValue(NUnitVersionKey, out nunitVersionOpt);
+        }
+
+        private void InitializeImpl(TestLoggerEvents events, string outputPath)
+        {
+            events.TestRunMessage += TestMessageHandler;
+            events.TestResult += TestResultHandler;
+            events.TestRunComplete += TestRunCompleteHandler;
+
+            outputFilePath = Path.GetFullPath(outputPath);
+
+            lock (resultsGuard)
+            {
+                results = new List<TestResultInfo>();
+            }
+
+            localStartTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Called when a test message is received.
+        /// </summary>
+        internal void TestMessageHandler(object sender, TestRunMessageEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Called when a test result is received.
+        /// </summary>
+        internal void TestResultHandler(object sender, TestResultEventArgs e)
+        {
+            TestResult result = e.Result;
+
+            if (TryParseName(result.TestCase.FullyQualifiedName, out var typeName, out var methodName, out _))
+            {
+                lock (resultsGuard)
+                {
+                    results.Add(new TestResultInfo(
+                        result,
+                        typeName,
+                        methodName));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when a test run is completed.
+        /// </summary>
+        internal void TestRunCompleteHandler(object sender, TestRunCompleteEventArgs e)
+        {
+            List<TestResultInfo> resultList;
+            lock (resultsGuard)
+            {
+                resultList = results;
+                results = new List<TestResultInfo>();
+            }
+
+            var doc = new XDocument(CreateAssembliesElement(resultList));
+
+            // Create directory if not exist
+            var loggerFileDirPath = Path.GetDirectoryName(outputFilePath);
+            if (!Directory.Exists(loggerFileDirPath))
+            {
+                Directory.CreateDirectory(loggerFileDirPath);
+            }
+
+            using (var f = File.Create(outputFilePath))
+            {
+                doc.Save(f);
+            }
+
+            var resultsFileMessage = string.Format(CultureInfo.CurrentCulture, "Results File: {0}", outputFilePath);
+            Console.WriteLine(resultsFileMessage);
+        }
+
+        private XElement CreateAssembliesElement(List<TestResultInfo> results)
+        {
+            var testSuites = from result in results
+                group result by result.AssemblyPath
+                into resultsByAssembly
+                orderby resultsByAssembly.Key
+                select CreateTestSuiteElement(resultsByAssembly);
+
+            var element = new XElement("test-run", testSuites);
+
+            element.SetAttributeValue("id", 2);
+
+            var resultString = DetermineResultFromDescendants(element, "test-suite");
+            element.SetAttributeValue("result", resultString);
+            element.SetAttributeValue("time", results.Sum(x => x.Time.TotalSeconds));
+
+            var total = testSuites.Sum(x => (int) x.Attribute("total"));
+
+            // TODO test case count is actually count before filtering
+            element.SetAttributeValue("testcasecount", total);
+            element.SetAttributeValue("total", total);
+            element.SetAttributeValue("passed", testSuites.Sum(x => (int) x.Attribute("passed")));
+            element.SetAttributeValue("failed", testSuites.Sum(x => (int) x.Attribute("failed")));
+            element.SetAttributeValue("skipped", testSuites.Sum(x => (int) x.Attribute("skipped")));
+
+            element.SetAttributeValue("run-date", localStartTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+            element.SetAttributeValue("run-time", localStartTime.ToString("HH:mm:ss", CultureInfo.InvariantCulture));
+
+            return element;
+        }
+
+        private static string DetermineResultFromDescendants(XElement element, string descendantElementName)
+        {
+            string resultString = ResultStatusPassed;
+            foreach (var x in element.Descendants(descendantElementName))
+            {
+                if (ResultStatusFailed.Equals((string) x.Attribute("result"), StringComparison.OrdinalIgnoreCase))
+                {
+                    resultString = ResultStatusFailed;
+                    break;
+                }
+            }
+            return resultString;
+        }
+
+        private XElement CreateTestSuiteElement(IGrouping<string, TestResultInfo> resultsByAssembly)
+        {
+            var assemblyPath = resultsByAssembly.Key;
+
+            var collections = from resultsInAssembly in resultsByAssembly
+                group resultsInAssembly by resultsInAssembly.Type
+                into resultsByType
+                orderby resultsByType.Key
+                select CreateTestSuite(resultsByType);
+
+            int total = 0;
+            int passed = 0;
+            int failed = 0;
+            int skipped = 0;
+            int errors = 0;
+            var time = TimeSpan.Zero;
+
+            var element = new XElement("test-suite");
+            element.SetAttributeValue("type", "Assembly");
+
+            XElement errorsElement = new XElement("errors");
+            element.Add(errorsElement);
+
+            foreach (var collection in collections)
+            {
+                total += collection.total;
+                passed += collection.passed;
+                failed += collection.failed;
+                skipped += collection.skipped;
+                errors += collection.error;
+                time += collection.time;
+
+                element.Add(collection.element);
+            }
+
+            element.SetAttributeValue("name", Path.GetFileName(assemblyPath));
+            element.SetAttributeValue("fullname", assemblyPath);
+
+            element.SetAttributeValue("total", total);
+            element.SetAttributeValue("passed", passed);
+            element.SetAttributeValue("failed", failed);
+            element.SetAttributeValue("skipped", skipped);
+            element.SetAttributeValue("time", time.TotalSeconds.ToString("N3", CultureInfo.InvariantCulture));
+            element.SetAttributeValue("errors", errors);
+
+            return element;
+        }
+
+        private static XElement CreateErrorElement(TestResultInfo result)
+        {
+            string errorMessage = result.ErrorMessage;
+
+            int indexOfErrorType = errorMessage.IndexOf('(');
+            errorMessage = errorMessage.Substring(indexOfErrorType + 1);
+
+            int indexOfName = errorMessage.IndexOf(')');
+            string name = errorMessage.Substring(0, indexOfName);
+            errorMessage = errorMessage.Substring(indexOfName + 4);
+
+            int indexOfExceptionType = errorMessage.IndexOf(':');
+            string exceptionType = errorMessage.Substring(0, indexOfExceptionType - 1);
+
+            XElement errorElement = new XElement("error");
+            errorElement.SetAttributeValue("name", name);
+
+            errorElement.Add(CreateFailureElement(exceptionType, errorMessage, result.ErrorStackTrace));
+
+            return errorElement;
+        }
+
+        private static XElement CreateFailureElement(string exceptionType, string message, string stackTrace)
+        {
+            XElement failureElement = new XElement("failure", new XAttribute("exception-type", exceptionType));
+            failureElement.Add(new XElement("message", RemoveInvalidXmlChar(message)));
+            failureElement.Add(new XElement("stack-trace", RemoveInvalidXmlChar(stackTrace)));
+
+            return failureElement;
+        }
+
+        private static (XElement element, int total, int passed, int failed, int skipped, int error, TimeSpan time) CreateTestSuite(IGrouping<string, TestResultInfo> resultsByType
+        )
+        {
+            var element = new XElement("test-suite");
+
+            int total = 0;
+            int passed = 0;
+            int failed = 0;
+            int skipped = 0;
+            int error = 0;
+            var time = TimeSpan.Zero;
+
+            foreach (var result in resultsByType)
+            {
+                switch (result.Outcome)
+                {
+                    case TestOutcome.Failed:
+                        failed++;
+                        break;
+
+                    case TestOutcome.Passed:
+                        passed++;
+                        break;
+
+                    case TestOutcome.Skipped:
+                    case TestOutcome.None:
+                        skipped++;
+                        break;
+                }
+
+                total++;
+                time += result.Time;
+
+                element.Add(CreateTestElement(result));
+            }
+
+            var name = resultsByType.Key;
+            var idx = name.LastIndexOf('.');
+            if (idx != -1)
+            {
+                name = name.Substring(idx + 1);
+            }
+            element.SetAttributeValue("type", "TestFixture");
+            element.SetAttributeValue("name", name);
+            element.SetAttributeValue("fullname", resultsByType.Key);
+
+            var resultString = DetermineResultFromDescendants(element, "test-case");
+            element.SetAttributeValue("result", resultString);
+
+            element.SetAttributeValue("total", total);
+            element.SetAttributeValue("passed", passed);
+            element.SetAttributeValue("failed", failed);
+            element.SetAttributeValue("skipped", skipped);
+            element.SetAttributeValue("time", time.TotalSeconds.ToString("N3", CultureInfo.InvariantCulture));
+
+            return (element, total, passed, failed, skipped, error, time);
+        }
+
+        private static XElement CreateTestElement(TestResultInfo result)
+        {
+            var element = new XElement("test-case",
+                new XAttribute("name", result.Name),
+                new XAttribute("fullname", result.Type + "." + result.Method),
+                new XAttribute("result", OutcomeToString(result.Outcome)),
+                new XAttribute("time", result.Time.TotalSeconds.ToString("N7", CultureInfo.InvariantCulture)),
+                new XAttribute("asserts", 0));
+
+            StringBuilder stdOut = new StringBuilder();
+            foreach (var m in result.Messages)
+            {
+                if (TestResultMessage.StandardOutCategory.Equals(m.Category, StringComparison.OrdinalIgnoreCase))
+                {
+                    stdOut.AppendLine(m.Text);
+                }
+            }
+
+            if (result.Outcome == TestOutcome.Failed)
+            {
+                element.Add(new XElement("failure",
+                    new XElement("message", RemoveInvalidXmlChar(result.ErrorMessage)),
+                    new XElement("stack-trace", RemoveInvalidXmlChar(result.ErrorStackTrace))));
+            }
+
+            return element;
+        }
+
+        private static bool TryParseName(string testCaseName, out string metadataTypeName, out string metadataMethodName, out string metadataMethodArguments)
+        {
+            // This is fragile. The FQN is constructed by a test adapter. 
+            // There is no enforcement that the FQN starts with metadata type name.
+
+            string typeAndMethodName;
+            var methodArgumentsStart = testCaseName.IndexOf('(');
+
+            if (methodArgumentsStart == -1)
+            {
+                typeAndMethodName = testCaseName.Trim();
+                metadataMethodArguments = string.Empty;
+            }
+            else
+            {
+                typeAndMethodName = testCaseName.Substring(0, methodArgumentsStart).Trim();
+                metadataMethodArguments = testCaseName.Substring(methodArgumentsStart).Trim();
+
+                if (metadataMethodArguments[metadataMethodArguments.Length - 1] != ')')
+                {
+                    metadataTypeName = null;
+                    metadataMethodName = null;
+                    metadataMethodArguments = null;
+                    return false;
+                }
+            }
+
+            var typeNameLength = typeAndMethodName.LastIndexOf('.');
+            var methodNameStart = typeNameLength + 1;
+
+            if (typeNameLength <= 0 || methodNameStart == typeAndMethodName.Length) // No typeName is available
+            {
+                metadataTypeName = null;
+                metadataMethodName = null;
+                metadataMethodArguments = null;
+                return false;
+            }
+
+            metadataTypeName = typeAndMethodName.Substring(0, typeNameLength).Trim();
+            metadataMethodName = typeAndMethodName.Substring(methodNameStart).Trim();
+            return true;
+        }
+
+        private static string OutcomeToString(TestOutcome outcome)
+        {
+            switch (outcome)
+            {
+                case TestOutcome.Failed:
+                    return ResultStatusFailed;
+
+                case TestOutcome.Passed:
+                    return "Passed";
+
+                case TestOutcome.Skipped:
+                    return "Skipped";
+
+                default:
+                    return "Inconclusive";
+            }
+        }
+
+        private static string RemoveInvalidXmlChar(string str)
+        {
+            if (str != null)
+            {
+                // From xml spec (http://www.w3.org/TR/xml/#charsets) valid chars: 
+                // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]  
+
+                // we are handling only #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD]
+                // because C# support unicode character in range \u0000 to \uFFFF
+                MatchEvaluator evaluator = new MatchEvaluator(ReplaceInvalidCharacterWithUniCodeEscapeSequence);
+                string invalidChar = @"[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD]";
+                return Regex.Replace(str, invalidChar, evaluator);
+            }
+
+            return str;
+        }
+
+        private static string ReplaceInvalidCharacterWithUniCodeEscapeSequence(Match match)
+        {
+            char x = match.Value[0];
+            return string.Format(@"\u{0:x4}", (ushort) x);
+        }
+    }
+}
