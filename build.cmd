151f3129aa8b8f6b56d13871ad010a0f9545401a
Arun Mahapatra <pratikarun@gmail.com>
2018-06-24 11:23:10 +0530

    test: add few tests for logger.

--- build.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-powershell -ExecutionPolicy Bypass -NoProfile -NoLogo -Command "%~dp0scripts\build.ps1 %*; exit $LastExitCode;" 
-if %errorlevel% neq 0 exit /b %errorlevel% 
---
+++ test/NUnit.Xml.TestLogger.AcceptanceTests/NUnitXmlTestLoggerTests.cs
@@ -0,0 +1,113 @@
+namespace NUnit.Xml.TestLogger.AcceptanceTests
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NUnitXmlTestLoggerTests
+    {
+        private readonly string resultsFile;
+        private readonly XDocument resultsXml;
+
+        public NUnitXmlTestLoggerTests()
+        {
+            this.resultsFile = Path.Combine(DotnetTestFixture.RootDirectory, "test-results.xml");
+            this.resultsXml = XDocument.Load(this.resultsFile);
+        }
+
+        [ClassInitialize]
+        public static void SuiteInitialize(TestContext context)
+        {
+            DotnetTestFixture.Execute();
+        }
+
+        [TestMethod]
+        public void TestRunWithLoggerAndFilePathShouldCreateResultsFile()
+        {
+            Assert.IsTrue(File.Exists(this.resultsFile));
+        }
+
+        [TestMethod]
+        public void TestResultFileShouldContainTestRunInformation()
+        {
+            var node = this.resultsXml.XPathSelectElement("/test-run");
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual("21", node.Attribute(XName.Get("testcasecount")).Value);
+            Assert.AreEqual("7", node.Attribute(XName.Get("passed")).Value);
+            Assert.AreEqual("7", node.Attribute(XName.Get("failed")).Value);
+            Assert.AreEqual("7", node.Attribute(XName.Get("skipped")).Value);
+            Assert.AreEqual("Failed", node.Attribute(XName.Get("result")).Value);
+
+            // Start time and End time should be valid dates
+            Convert.ToDateTime(node.Attribute(XName.Get("start-time")).Value);
+            Convert.ToDateTime(node.Attribute(XName.Get("end-time")).Value);
+        }
+
+        [TestMethod]
+        public void TestResultFileShouldContainAssemblyTestSuite()
+        {
+            var node = this.resultsXml.XPathSelectElement("/test-run/test-suite[@type='Assembly']");
+        }
+    }
+
+    public class DotnetTestFixture
+    {
+        public static string RootDirectory
+        {
+            get
+            {
+                return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory,
+                                                     "..",
+                                                     "..",
+                                                     "..",
+                                                     "..",
+                                                     "assets",
+                                                     "NUnit.Xml.TestLogger.NetCore.Tests"));
+            }
+        }
+
+        public static void Execute()
+        {
+            var testProject = RootDirectory;
+            var testLogger = $"--logger:\"nunit;LogFilePath=test-results.xml\"";
+
+            // Delete stale results file
+            var testLogFile = Path.Combine(testProject, "test-results.xml");
+            if (File.Exists(testLogFile))
+            {
+                File.Delete(testLogFile);
+            }
+
+            // Log the contents of test output directory. Useful to verify if the logger is copied
+            Console.WriteLine("------------");
+            Console.WriteLine("Contents of test output directory:");
+            foreach (var f in Directory.GetFiles(Path.Combine(testProject, "bin/Debug/netcoreapp2.0")))
+            {
+                Console.WriteLine("  " + f);
+            }
+            Console.WriteLine();
+
+            // Run dotnet test with logger
+            using (var p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = "dotnet";
+                p.StartInfo.Arguments = $"test --no-build {testLogger} {testProject}";
+                p.Start();
+
+                Console.WriteLine("dotnet arguments: " + p.StartInfo.Arguments);
+                // To avoid deadlocks, always read the output stream first and then wait.
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                Console.WriteLine("dotnet output: " + output);
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
