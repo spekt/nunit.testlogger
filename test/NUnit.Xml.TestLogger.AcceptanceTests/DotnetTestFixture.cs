ab62f03e7df95ceea3d4178a9ad5ae8cf1f7fef7
ashishdawale20 <ashishdawale20@gmail.com>
2019-06-11 22:19:54 +0530

    Acceptance test for Respect ResultsDirectory #46

--- test/NUnit.Xml.TestLogger.AcceptanceTests/DotnetTestFixture.cs
+++ test/NUnit.Xml.TestLogger.AcceptanceTests/DotnetTestFixture.cs
@@ -19,6 +19,17 @@ namespace NUnit.Xml.TestLogger.AcceptanceTests
                         "assets",
                         "NUnit.Xml.TestLogger.NetCore.Tests"));
 
+        public static string ResultDirectory { get; set; } = Path.GetFullPath(
+                 Path.Combine(
+                     Environment.CurrentDirectory,
+                     "..",
+                     "..",
+                     "..",
+                     "..",
+                     "assets",
+                     "NUnit.Xml.TestLogger.NetCore.Tests",
+                     "artifacts"));
+
         public static string TestAssemblyName { get; set; } = "NUnit.Xml.TestLogger.NetCore.Tests.dll";
 
         public static string TestAssembly
@@ -51,6 +62,40 @@ namespace NUnit.Xml.TestLogger.AcceptanceTests
                     File.Delete(fileName);
                 }
             }
+
+            // Log the contents of test output directory. Useful to verify if the logger is copied
+            Console.WriteLine("------------");
+            Console.WriteLine("Contents of test output directory:");
+            foreach (var f in Directory.GetFiles(Path.Combine(testProject, "bin/Debug/netcoreapp2.0")))
+            {
+                Console.WriteLine("  " + f);
+            }
+
+            Console.WriteLine();
+
+            // Run dotnet test with logger
+            using (var p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = "dotnet";
+                p.StartInfo.Arguments = $"test --no-build {testLogger} {testProject}";
+                p.Start();
+
+                Console.WriteLine("dotnet arguments: " + p.StartInfo.Arguments);
+
+                // To avoid deadlocks, always read the output stream first and then wait.
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                Console.WriteLine("dotnet output: " + output);
+                Console.WriteLine("------------");
+            }
+        }
+
+        public static void Execute(string resultsFileName, string filePath)
+        {
+            var testProject = RootDirectory;
+            var testLogger = $"--logger \"nunit;LogFileName={resultsFileName}\" --results-directory \"{filePath}\"";
 
             // Log the contents of test output directory. Useful to verify if the logger is copied
             Console.WriteLine("------------");
---
+++ test/NUnit.Xml.TestLogger.AcceptanceTests/NUnitTestLoggerResultDirectoryAcceptanceTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Spekt Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NUnit.Xml.TestLogger.AcceptanceTests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NUnitTestLoggerResultDirectoryAcceptanceTests
+    {
+        private readonly string resultsFile;
+        private readonly XDocument resultsXml;
+
+        public NUnitTestLoggerResultDirectoryAcceptanceTests()
+        {
+            this.resultsFile = Path.Combine(DotnetTestFixture.ResultDirectory, "test-results.xml");
+            this.resultsXml = XDocument.Load(this.resultsFile);
+        }
+
+        [ClassInitialize]
+        public static void SuiteInitialize(TestContext context)
+        {
+            DotnetTestFixture.Execute("test-results.xml", "./artifacts");
+        }
+
+        [TestMethod]
+        public void TestRunWithResultDirectoryAndFileNameShouldCreateResultsFile()
+        {
+            Assert.IsTrue(File.Exists(this.resultsFile));
+        }
+    }
+}
