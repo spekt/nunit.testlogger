a517b76f326b29c5bf723fdd503730e18d72150a
Arun Mahapatra <codito@codito.in>
2023-07-04 17:05:39 +0530

    fix: use latest testlogger core.

--- src/NUnit.Xml.TestLogger/NUnitXmlSerializer.cs
+++ src/NUnit.Xml.TestLogger/NUnitXmlSerializer.cs
@@ -253,8 +253,8 @@ namespace Microsoft.VisualStudio.TestPlatform.Extension.NUnit.Xml.TestLogger
                 new XAttribute("end-time", result.EndTime.ToString(DateFormat, CultureInfo.InvariantCulture)),
                 new XAttribute("duration", result.Duration.TotalSeconds),
                 new XAttribute("asserts", 0),
-                CreateSeedAttribute(result.TestCase),
-                CreatePropertiesElement(result.TestCase));
+                CreateSeedAttribute(result),
+                CreatePropertiesElement(result));
 
             StringBuilder stdOut = new StringBuilder();
             foreach (var m in result.Messages)
@@ -281,15 +281,15 @@ namespace Microsoft.VisualStudio.TestPlatform.Extension.NUnit.Xml.TestLogger
             return element;
         }
 
-        private static XAttribute CreateSeedAttribute(TestCase result)
+        private static XAttribute CreateSeedAttribute(TestResultInfo result)
         {
-            var seed = result.Properties.SingleOrDefault(p => p.Id == "NUnit.Seed");
-            return seed != null
-                ? new XAttribute("seed", result.GetPropertyValue(seed))
+            var seed = result.Properties.SingleOrDefault(p => p.Key == "NUnit.Seed");
+            return seed.Key == "NUnit.Seed"
+                ? new XAttribute("seed", seed.Value)
                 : null;
         }
 
-        private static XElement CreatePropertiesElement(TestCase result)
+        private static XElement CreatePropertiesElement(TestResultInfo result)
         {
             var propertyElements = new HashSet<XElement>(result.Traits.Select(CreatePropertyElement));
 
@@ -297,16 +297,15 @@ namespace Microsoft.VisualStudio.TestPlatform.Extension.NUnit.Xml.TestLogger
 
             // Required since TestCase.Properties is a superset of TestCase.Traits
             // Unfortunately not all NUnit properties are available as traits
-            var traitProperties = result.Properties.Where(t => t.Attributes.HasFlag(TestPropertyAttributes.Trait));
+            var traitProperties = result.Properties;
 
 #pragma warning restore CS0618 // Type or member is obsolete
 
             foreach (var p in traitProperties)
             {
-                var propValue = result.GetPropertyValue(p);
-
-                if (p.Id == "NUnit.TestCategory")
+                if (p.Key == "NUnit.TestCategory")
                 {
+                    var propValue = p.Value;
                     var elements = CreatePropertyElement("Category", (string[])propValue);
 
                     foreach (var element in elements)
